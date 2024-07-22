6d34921ddce2143327d049c6c9ea9100e55e6b75
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-07-22 21:02:42 +0300

    временно отключены настройки визуализации парсера

--- app/parser/utils.py
+++ app/parser/utils.py
@@ -7,14 +7,14 @@ def get_chrome_driver_options() -> Options:
     """Получить параметры для запуска браузера Chrome."""
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--headless=new")
-    options.add_argument("--start-maximized")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--allow-running-insecure-content")
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--headless=new")
+    # options.add_argument("--start-maximized")
+    # options.add_argument("--ignore-certificate-errors")
+    # options.add_argument("--allow-running-insecure-content")
     user_agent = UserAgent(browsers=["chrome"]).getRandom.get("useragent")
     options.add_argument(f"user-agent={user_agent}")
     return options
