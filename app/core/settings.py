39a07a8b895a8cd03dea0cc5166aa3805cfb0c33
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-07-28 21:26:18 +0300

    добавлен функционал который результаты заносит в заданную папку

--- app/core/settings.py
+++ app/core/settings.py
@@ -2,6 +2,8 @@
 
 import os
 
+from pydantic import field_validator
+
 from core.types import LOG_LEVEL
 
 from pydantic_settings import BaseSettings
@@ -31,3 +33,14 @@ class LogSettings(Base):
     level: LOG_LEVEL = "INFO"
     guru: bool = True
     traceback: bool = True
+
+
+class CSVSettings(Base):
+    csv_dir: str = os.path.join(BASE_DIR, "app/csv")
+
+    @field_validator("csv_dir", mode="before")
+    def _(cls, v: str) -> str:  # noqa:
+        csv_dir = os.path.join(BASE_DIR, "app/csv", v)
+        if not os.path.exists(csv_dir):
+            os.makedirs(csv_dir)
+        return os.path.join(BASE_DIR, "app/csv", v)
--- app/core/utils.py
+++ app/core/utils.py
@@ -1,8 +1,13 @@
 import csv
+import os
+
+from core.settings import CSVSettings
+
+CSV_DIR = CSVSettings().csv_dir
 
 
 def save_data_to_csv(data: dict, filename="data.csv"):
-    with open(filename, "w", newline="") as csvfile:
+    with open(os.path.join(CSV_DIR, filename), "w", newline="") as csvfile:
         fieldnames = data.keys()
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
